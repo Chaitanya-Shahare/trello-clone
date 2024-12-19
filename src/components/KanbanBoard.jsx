d826d3fc931f15f418f8f25d7c54d912f76bc056
Chaitanya Shahare <shaharechaitanya3@gmail.com>
2024-12-19 18:05:46 +0530

    Add & delete card & list done

--- src/components/KanbanBoard.jsx
+++ src/components/KanbanBoard.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import "./KanbanBoard.scss";
 import List from "./List";
+import { nanoid } from "nanoid";
 
 function KanbanBoard() {
   // array of objects of type list
@@ -41,17 +42,46 @@ function KanbanBoard() {
     setBoard(dummyBoard);
   };
 
-  // list
-  //   {
-  //   id:
-  //   title:
-  //   cards: [
-  //   {
-  //   id:
-  //   title:
-  //     }
-  //   ]
-  //     }
+  const [isCardModalOpen, setIsCardModalOpen] = useState(false);
+  const [isCreateListModalOpen, setIsCreateListModalOpen] = useState(false);
+
+  const [listIndex, setListIndex] = useState(0);
+  const [title, setTitle] = useState("");
+
+  const handleAddCard = (e) => {
+    e.preventDefault();
+    setIsCardModalOpen(false);
+    let newboard = [...board];
+    newboard[listIndex].cards.push({
+      id: nanoid(),
+      title,
+    });
+    setTitle("");
+    setBoard(newboard);
+  };
+
+  const toggleCardModal = (index) => {
+    setListIndex(index);
+    setIsCardModalOpen(true);
+  };
+
+  const handleCreateList = (e) => {
+    e.preventDefault();
+    setIsCreateListModalOpen(false);
+    setBoard((prev) => [
+      ...prev,
+      {
+        id: nanoid(),
+        title: title,
+      },
+    ]);
+    setTitle("");
+  };
+
+  const deleteList = (listId) => {
+    let newBoard = board.filter((list) => list.id !== listId);
+    setBoard(newBoard);
+  };
 
   useEffect(() => {
     initializeBoard();
@@ -59,12 +89,63 @@ function KanbanBoard() {
 
   return (
     <>
-      <button className="create-list-btn">Create List</button>
+      <button
+        className="create-list-btn"
+        onClick={() => setIsCreateListModalOpen(true)}
+      >
+        Create List
+      </button>
       <div className="board">
-        {board.map((list) => {
-          return <List key={list.id} title={list.title} cards={list.cards} />;
+        {board.map((list, index) => {
+          return (
+            <List
+              key={list.id}
+              id={list.id}
+              title={list.title}
+              cards={list.cards}
+              toggleCardModal={() => {
+                toggleCardModal(index);
+                setIsCardModalOpen(true);
+              }}
+              deleteList={deleteList}
+            />
+          );
         })}
       </div>
+
+      {isCardModalOpen && (
+        <div className="modal">
+          <form className="modal__details" onSubmit={handleAddCard}>
+            <label htmlFor="card-title-input"></label>
+            Card Title
+            <input
+              type="text"
+              id="card-title-input"
+              value={title}
+              autoFocus
+              onChange={(e) => setTitle(e.target.value)}
+            />
+            <input type="submit" value="Add Card" />
+          </form>
+        </div>
+      )}
+
+      {isCreateListModalOpen && (
+        <div className="modal">
+          <form className="modal__details" onSubmit={handleCreateList}>
+            <label htmlFor="list-title-input"></label>
+            List Title
+            <input
+              type="text"
+              id="list-title-input"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              autoFocus
+            />
+            <input type="submit" value="Add List" />
+          </form>
+        </div>
+      )}
     </>
   );
 }
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,21 +1,17 @@
 import React from "react";
 import "./List.scss";
 import Card from "./Card";
-import {
-  RiAddFill,
-  RiDeleteBack2Fill,
-  RiDeleteBin2Fill,
-} from "@remixicon/react";
+import { RiAddFill, RiDeleteBin2Fill } from "@remixicon/react";
 
-function List({ title, cards = [] }) {
+function List({ id, title, cards = [], toggleCardModal, deleteList }) {
   return (
     <div className="list">
       <div className="list__header">
         <h3 className="list__title">{title}</h3>
-        <button className="list__add-btn">
+        <button className="list__add-btn" onClick={toggleCardModal}>
           <RiAddFill size={20} />
         </button>
-        <button className="card__delete-btn">
+        <button className="card__delete-btn" onClick={() => deleteList(id)}>
           <RiDeleteBin2Fill size={20} />
         </button>
       </div>
